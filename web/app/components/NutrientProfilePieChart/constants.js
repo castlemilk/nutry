7370c9013ae611132b7d5a5d6ce26eb26eccfb0c
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-01-27 17:21:35 +1100

    perf integrated

---
+++ web/app/components/NutrientProfilePieChart/constants.js
@@ -0,0 +1,18 @@
+export const FILTERS = {
+  summary: [
+    'CHOCDF',
+    'SUGAR',
+    'FIBTG',
+    'PROCNT',
+    'FAT',
+    'WATER',
+  ],
+  detailed: [
+    'CHOCDF',
+    'SUGAR',
+    'FIBTG',
+    'PROCNT',
+    'FAT',
+    'WATER',
+  ],
+};
