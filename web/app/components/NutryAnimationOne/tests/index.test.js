73c64f98b1132b55fb08b2107e0887a0d8015b6f
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-02-20 08:32:05 +1100

    completed test coverage for NutryAnimationOne component

--- web/app/components/NutryAnimationOne/tests/index.test.js
+++ web/app/components/NutryAnimationOne/tests/index.test.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 // import sinon from 'sinon';
-
+import { getXPosition, getYPosition } from 'lib/utils';
 import NutryAnimationOne from '../index';
 import NutryAnimationOneWrapper from '../NutryAnimationOneWrapper';
 
 describe('<NutryAnimationOne />', () => {
   it('should render a div', () => {
+    const radius = 20;
     const renderedComponent = shallow(
       <NutryAnimationOne />
     );
-    console.log(renderedComponent.debug());
-    console.log(renderedComponent.props);
     expect(renderedComponent.find(NutryAnimationOneWrapper).length).toEqual(1);
     expect(renderedComponent.find('Anime').length).toEqual(4);
     expect(renderedComponent.find('img').length).toEqual(14);
     expect(renderedComponent).toMatchSnapshot();
+    expect(renderedComponent.find('Anime').get(2).props.delay(null, 1)).toEqual(240);
+    expect(renderedComponent.find('Anime').get(2).props.translateX(null, 1)).toEqual(`${getXPosition(radius, 1)}rem`);
+    expect(renderedComponent.find('Anime').get(2).props.translateY(null, 1)).toEqual(`${getYPosition(radius, 1)}rem`);
+    expect(renderedComponent.find('Anime').get(3).props.delay(null, 1)).toEqual(440);
+    expect(renderedComponent.find('Anime').get(3).props.translateX(null, 1)).toEqual(`${getXPosition(radius, 1)}rem`);
+    expect(renderedComponent.find('Anime').get(3).props.translateY(null, 1)).toEqual(`${getYPosition(radius, 1)}rem`);
     // console.log(renderedComponent.find('Anime').get(3).props.delay);
   });
 });
