218ae02eba50cf1d1d3e6715e5846c0d77a6cd46
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-02-21 06:16:50 +1100

    added test coverage for ProfilerAddElement component

--- web/app/components/ProfilerAddElement/tests/index.test.js
+++ web/app/components/ProfilerAddElement/tests/index.test.js
@@ -1,10 +1,28 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-
-// import ProfilerAddElement from '../index';
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProfilerAddElement from '../index';
+import ProfilerAddElementWrapper from '../ProfilerAddElementWrapper';
 
 describe('<ProfilerAddElement />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should render', () => {
+    const props = {
+      onHover: () => {},
+    };
+    const renderedComponent = shallow(
+      <ProfilerAddElement {...props} />
+    );
+    expect(renderedComponent.find(ProfilerAddElementWrapper).length).toEqual(1);
+  });
+  it('click effect works', () => {
+    const onClickSpy = jest.fn();
+    const props = {
+      onClick: onClickSpy,
+    };
+    const renderedComponent = shallow(
+      <ProfilerAddElement {...props} />
+    );
+    expect(renderedComponent.find(ProfilerAddElementWrapper).length).toEqual(1);
+    renderedComponent.find('button').simulate('click');
+    expect(onClickSpy).toHaveBeenCalled();
   });
 });
