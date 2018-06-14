12175b101f3c0618a858207ff0e92b4280bd033c
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-06-14 20:28:51 +1000

    working production container with injectable environment variables

--- web/app/config.js
+++ web/app/config.js
@@ -1,5 +1,5 @@
 export const elasticsearchConfig = {
-  hostname: `${process.env.ELASTICSEARCH_PROXY_MODE}://${process.env.ELASTICSEARCH_PROXY_HOST}:${process.env.ELASTICSEARCH_PROXY_PORT}/`,
+  hostname: `${process.env.ELASTICSEARCH_PROXY_MODE}://${process.env.ELASTICSEARCH_PROXY_HOST}:${process.env.ELASTICSEARCH_PROXY_PORT}${process.env.ELASTICSEARCH_PROXY_PREFIX}/`,
   indexNames: 'names',
   indexNutrients: 'nutrients',
   authentication: {
